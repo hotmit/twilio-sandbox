74f00cff978a75e8632caa4cdfe0e4ebaaebcdb2
Du Dang <hmlinks@gmail.com>
2017-02-24 11:35:41 -0500

    added requirements.txt file

--- live/settings.py
+++ live/settings.py
@@ -1,8 +1,14 @@
+from os import path
 from twilio_sandbox.settings import *
+
 
 DEBUG = True
 CRISPY_FAIL_SILENTLY = not DEBUG
-COMPRESS_ENABLED = not DEBUG		                        # enable grouping and minification
-COMPRESS_OFFLINE = True				                        # allow compress file 'manage.py compress'
+COMPRESS_ENABLED = not DEBUG        # enable grouping and minification
+COMPRESS_OFFLINE = True				# allow compress file 'manage.py compress'
 
 WSGI_APPLICATION = 'live.wsgi.application'
+
+BASE_URL = path.dirname(path.dirname(__file__))
+STATIC_ROOT = path.join(BASE_URL, 'static')
+MEDIA_ROOT = path.join(BASE_URL, 'media')
